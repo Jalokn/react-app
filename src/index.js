cbbcbd8d4d2b7fc218d2582832b98fb2ade6d95d
Jacob Knudson <jacoblknudson@gmail.com>
2022-07-01 14:08:55 -0600

    Learning React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import "./index.css";
+
+const BookList = () => {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+};
+
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L.jpg"
+    alt="I love you to the moon and back: coverArt"
+  />
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Title = () => <h1>I Love You to the Moon and Back</h1>;
+const Author = () => (
+  <h4 style={{ color: "#617d98", fontSize: "1rem", marginTop: ".25" }}>
+    Amelia Hepworth
+  </h4>
+);
+
+ReactDOM.render(<BookList />, document.getElementById("root"));
